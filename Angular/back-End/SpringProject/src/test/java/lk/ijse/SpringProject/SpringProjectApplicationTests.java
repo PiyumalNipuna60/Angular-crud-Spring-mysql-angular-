c4eef38926e1cdf45188f992f526e09974d45ea0
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-08-03 01:32:21 +0530

    first commit

---
+++ Angular/back-End/SpringProject/src/test/java/lk/ijse/SpringProject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.SpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
